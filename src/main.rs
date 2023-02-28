51fa62271d2ecab25950eaa27a65c60405256c47
robertsweetman <robert.sweetman@digital.justice.gov.uk>
2023-02-28 18:18:49 +0000

    pass value

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+// fn main() {
+//     let mut name = "Rob"; //Rust non owned String type, immutable by default. If you want to own the memory i.e. need to return a String from a function then use String instead.
+//     println!("Hello, {}!", name);
+//     name = "Bill";              // if you don't make 'name' mutable then you can't re-assign it to something else later
+//                                 // In fact the vscode editor will actually point this out - probably Rust Analyzer doing this!
+//     println!("hello, {}!", name);
+// }
+
+fn show(x: &str) {
+    println!("Hi, {}", x)
+}
+
 fn main() {
-    let mut name = "Rob"; //Rust non owned String type, immutable by default. If you want to own the memory i.e. need to return a String from a function then use String instead.
-    println!("Hello, {}!", name);
-    name = "Bill";              // if you don't make 'name' mutable then you can't re-assign it to something else later
-                                // In fact the vscode editor will actually point this out - probably Rust Analyzer doing this!
-    println!("hello, {}!", name);
+    let name = "Rob";
+    show(name);
 }
